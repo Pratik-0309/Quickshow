4d9d884369ef95202f6e8aed99fcb721795114cc
PratikParate <paratepratik10@gmail.com>
2026-01-12 22:50:12 +0530

    add conditional Navbar/Footer and Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Home from './pages/Home.jsx';
 import Movies from "./pages/Movies.jsx";
@@ -6,11 +6,17 @@ import MovieDetails from "./pages/MovieDetails.jsx";
 import SeatLayout from "./pages/SeatLayout.jsx";
 import MyBooking from './pages/MyBooking.jsx';
 import Favourite from './pages/Favourite.jsx';
+import {Toaster } from 'react-hot-toast';
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
     <>
-      <Navbar />
+      <Toaster />
+      { !isAdminRoute && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
@@ -19,6 +25,7 @@ const App = () => {
         <Route path="/my-bookings" element={<MyBooking />} />
         <Route path="/favourite" element={<Favourite />} />
       </Routes>
+      { !isAdminRoute && <Footer />}
     </>
   );
 };
