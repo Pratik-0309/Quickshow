ea6e7e3368837955b077d2e17e9291dc914241e8
PratikParate <paratepratik10@gmail.com>
2026-01-15 10:47:39 +0530

    render Hero, Features & Trailer Section components in Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Herosection from '../components/Herosection'
+import FeatureSection from '../components/FeatureSection'
+import TrailerSection from '../components/TrailerSection'
+
+const Home = () => {
+  return (
+    <>
+      <Herosection />
+      <FeatureSection />
+      <TrailerSection />
+    </>
+  )
+}
+
+export default Home
