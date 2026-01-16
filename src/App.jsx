3ba776b0fabdd14a12bb8b2751eccd511027a96a
PratikParate <paratepratik10@gmail.com>
2026-01-16 10:13:44 +0530

    add admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import MyBooking from './pages/MyBooking.jsx';
 import Favourite from './pages/Favourite.jsx';
 import {Toaster } from 'react-hot-toast';
 import Footer from "./components/Footer.jsx";
+import Layout from "./pages/Admin/Layout.jsx";
+import DashBoard from "./pages/Admin/DashBoard.jsx";
+import AddShow from "./pages/Admin/AddShow.jsx";
+import ListShow from "./pages/Admin/ListShow.jsx";
+import ListBooking from "./pages/Admin/ListBooking.jsx";
 
 const App = () => {
 
@@ -24,6 +29,12 @@ const App = () => {
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBooking />} />
         <Route path="/favourite" element={<Favourite />} />
+        <Route path="/admin/*" element={<Layout />}>
+        <Route index element={<DashBoard />} />
+        <Route path="add-shows" element={<AddShow />} />
+        <Route path="list-shows" element={<ListShow />} />
+        <Route path="list-bookings" element={<ListBooking />} />
+        </Route>
       </Routes>
       { !isAdminRoute && <Footer />}
     </>
