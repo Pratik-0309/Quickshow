a08d329b3b4b14680211b541686c4e59269b4d27
PratikParate <paratepratik10@gmail.com>
2026-01-17 11:21:38 +0530

    integrate Clerk middleware & Inngest server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser';
 import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 connectDB();
-
+ 
 app.use(cookieParser());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -20,6 +24,8 @@ app.use(cors({
 app.get("/",(req,res)=> {
     res.send("Hello World");
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(process.env.PORT || 8080,(req,res)=> {
     console.log(`Server is listening on port ${process.env.PORT || 8080}`)
