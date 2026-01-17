437d452f2638c63f859679e2cd6b63838f1d7ef0
PratikParate <paratepratik10@gmail.com>
2026-01-17 10:12:31 +0530

    setup Express server with some middleware

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import cookieParser from 'cookie-parser';
+import { connectDB } from './config/db.js';
+
+const app = express();
+connectDB();
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ["GET","POST","PUT","DELETE"],
+    allowedHeaders: ["Content-Type","Authorization"],
+}))
+
+app.get("/",(req,res)=> {
+    res.send("Hello World");
+})
+
+app.listen(process.env.PORT || 8080,(req,res)=> {
+    console.log(`Server is listening on port ${process.env.PORT || 8080}`)
+})
