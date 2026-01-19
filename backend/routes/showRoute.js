bf83c8a84832d2437c60258a22b87b80c0950e81
PratikParate <paratepratik10@gmail.com>
2026-01-19 22:12:24 +0530

    add route and verifyAdmin middleware

--- backend/routes/showRoute.js
+++ backend/routes/showRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { addShow, getNowPlayingMovies } from '../controller/showController.js';
+import { addShow, getNowPlayingMovies,getShows,getShow } from '../controller/showController.js';
+import { verfiyAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get("/now-playing",getNowPlayingMovies);
-router.post("/add",addShow); 
-
+router.get("/now-playing",verfiyAdmin,getNowPlayingMovies);
+router.post("/add",verfiyAdmin,addShow); 
+router.get("/all",getShows);
+router.get("/:movieId",getShow)
 export default router;
