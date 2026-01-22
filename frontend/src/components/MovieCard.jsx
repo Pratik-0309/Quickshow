8b3a10584c367070cc9ccbbcf70aa6d4d4fce97a
PratikParate <paratepratik10@gmail.com>
2026-01-22 23:44:31 +0530

    refactor MovieCard to use image_base_url and format runtime

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,23 +1,27 @@
 import { StarIcon } from 'lucide-react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useAppContext } from '../context/AppContext';
+import timeFormat from '../lib/timeFormat';
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie: show}) => {
 
     const navigate = useNavigate();
+    const {image_base_url} = useAppContext();
+    const movie = show.movie;
 
   return (
     <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1
     transition duration-300 w-66'>
        <img onClick={()=> {navigate(`/movies/${movie._id}`); scroll(0,0)} }
-       src={movie.backdrop_path} alt="" 
+       src={image_base_url + movie.backdrop_path} alt="" 
        className='rounded-lg h-52 w-full object-cover object-bottom-right cursor-pointer'
        />
        <p className='font-semibold mt-2 truncate'>{movie.title}</p>
        <p className='text-sm text-gray-400 mt-2'>
-        {new Date(movie.release_date).getFullYear()} •
-         {movie.genres.slice(0,2).map(genre => genre.name).join(" | ")} •
-         {movie.runtime}
+        {new Date(movie.release_date).getFullYear()}  • {" "}
+         { movie.genres.slice(0,2).map(genre => genre.name).join(" | ")}  • {" "}
+         {timeFormat(movie.runtime)}
        </p>
        <div className='flex items-center justify-between mt-4 pb-3'>
         <button onClick={()=> {navigate(`/movies/${movie._id}`); scroll(0,0)} }
