fe1b95eb6784eadb76607e5e349e636694567b46
PratikParate <paratepratik10@gmail.com>
2026-01-25 11:29:18 +0530

    add confirmation email sending on successful payment intent

--- backend/controller/stripeWebhook.js
+++ backend/controller/stripeWebhook.js
@@ -1,5 +1,6 @@
 import stripe from "stripe";
 import Booking from "../model/Booking.js";
+import { inngest } from "../inngest/index.js";
 
 export const stripeWebhooks = async (request, response) => {
   const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
@@ -31,6 +32,13 @@ export const stripeWebhooks = async (request, response) => {
           isPaid: true,
           paymentLink: "",
         });
+
+        //send confirmation email
+        await inngest.send({
+          name: "app/show.booked",
+          data: {bookingId}
+        })
+
         break;
       }
 
