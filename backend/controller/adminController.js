8febe52c76dd677753b40711541b77f4660990ec
PratikParate <paratepratik10@gmail.com>
2026-01-25 12:07:27 +0530

    fix: correct totalRevenue calculation in getDashboardData function

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -20,7 +20,7 @@ const getDashboardData = async (req, res) => {
 
     const dashboardData = {
       totalBookings: bookings.length,
-      totalRevenue: bookings.reduce((acc, booking) => acc.booking.amount, 0),
+      totalRevenue: bookings.reduce((acc, booking) => acc + (booking.amount || 0), 0),
       activeShows,
       totalUser,
     };
