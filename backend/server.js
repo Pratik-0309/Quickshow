02f4483d6796f38c989fc0f22c3daba5685c5dbd
PratikParate <paratepratik10@gmail.com>
2026-01-26 10:23:34 +0530

    fix: update CORS origin to allow requests from production URL

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(clerkMiddleware())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin:[ 'http://localhost:5173', 'https://quickshow-puce-omega.vercel.app'],
     credentials: true,
     methods: ["GET","POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type","Authorization"],
